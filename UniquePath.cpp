eabf9f0255956372a185ab35c76a19627bcaa951
cptfalcn <jaredjstewart@gmail.com>
2026-01-16 09:20:17 -0800

    Final changes to the leet code problem 62

--- UniquePath.cpp
+++ UniquePath.cpp
@@ -2,34 +2,40 @@
 #define box "\u25A0"
 #define path "\u25AA"
 
-//Prone to overflow
-// int fact(int n){
-//      return (n==0) || (n==1) ? 1 : n* fact(n-1);
-// }
-
-// int Choose(int n, int r){
-//     return fact(n)/(fact(r)* fact(n -r));
-// }
-
-
-//This is the version that is less prone to overflow because addition is used, not mult.
-int fastBin(int n, int m){
-    int result = 0;
-    if( (n==1) or (m == 1) or n ==0 or m ==0 )//Base case
-        return 1;
-    else 
-        return fastBin(m, n -1) + fastBin(m-1, n);//Recursion case
-}
-
 
 class Solution {
 public:
+    /*
+        The unique path problem (Leetcode problem 62) is equivalent to finding 
+        the binomial coefficients of m choose n.  Brute forcing the formula
+        results in integer overflows due to repeated multiplication via the
+        factorial function.  To mitigate overflow recursion and pascal's 
+        triangle is used to add integers instead of multiplying.
+    */
     int uniquePaths(int m, int n) {
-        //return Choose(m+n-2, m-1);
         return fastBin(m,n);
     }
-    //Visualizes the maze, uses m and n.
-    //Calls the two other draw functions and the macro defined ANSCII codes.
+
+    /*====================================
+    //Fast Binomial Coefficients
+    //Using recursion the binomial coefficients for m choose n are calculated
+    //Inputs:  m, n.
+    //Output:  m choose n
+    //======================================
+    */
+
+    int fastBin(int n, int m){
+        int result = 0;
+        if( (n==1) or (m == 1) or n ==0 or m ==0 )//Base case
+            return 1;
+        else 
+        return fastBin(m, n -1) + fastBin(m-1, n);//Recursion case
+    }
+    /*===================================================
+    //This is a purely ornamental addition to visualize the maze onto the console,
+    //and is not part of the original problem.
+    //===================================================
+    */
     void drawBox(int m, int n){
         this->drawTopBot(m);
         for (int i = 0; i < n; i++)
@@ -52,10 +58,9 @@ public:
 
 int main(void){
     Solution sol;
-    int m = 8;
-    int n = 5;
+    int m = 3;
+    int n = 7;
     sol.drawBox(m,n);
-    //std:: cout << sol.uniquePaths(m,n)<< " unique traversals paths exist."<<std::endl;
-    std:: cout << fastBin(m,n) << " unique traversal paths exist." << std::endl;
+    std:: cout << sol.fastBin(m,n) << " unique traversal paths exist." << std::endl;
     return 0;
 }
